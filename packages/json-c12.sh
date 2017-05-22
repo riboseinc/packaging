0c9182aae5a40ff7ef2ba4a62b0927b74779ded5
Ronald Tse <ronald.tse@ribose.com>
2017-05-23 03:01:26 +0800

    Update json-c12 script to be consistent with the rest.

--- packages/json-c12.sh
+++ packages/json-c12.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-package_name=json-c12
-
-base=$(cd "$(dirname "$0")"; pwd)
-root=$(cd "${base}/../"; pwd)
-echo "base is ${base}"
-echo "root is ${root}"
-
-${base}/_common.sh
-
-repo_name=rpm-specs
-repo_path=/usr/local/${repo_name}
-if [ ! -d ${repo_path} ] && [ ! -d ${repo_path}/.git ]; then
-  git clone --depth 1 https://github.com/riboseinc/${repo_name} ${repo_path}
-fi
-
-#pushd ${repo_path}/${package_name}
-
-# TODO: change rpm-specs repo to use a consistent name in prepare.sh
-package_path=/usr/local/${package_name}
-mkdir -p ${package_path}
-cp -ra ${repo_path}/${package_name}/* ${package_path}/
-pushd ${package_path}
-
-./prepare.sh
-
-${root}/publish_rpmbuild_to_yum.sh
+base=$(cd "$(dirname "$0")/.."; pwd)
+. ${base}/scripts/_packaging.sh
+the_works json-c12
 
