ed91f99f61ab03c72c48ba78d1c2facc77fb1c37
Ronald Tse <ronald.tse@ribose.com>
2017-05-23 01:20:18 +0800

    Restructure and cleanup of scripts.

--- packages/_common.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yum install -y git createrepo
-
-base=$(cd "$(dirname "$0")"; pwd)
-root=$(cd "${base}/../"; pwd)
-
-${root}/set_yum_push_credentials.sh ${REPO_USERNAME} ${REPO_PASSWORD}
-${root}/import_packaging_key.sh ${PACKAGER_KEY_PATH}
