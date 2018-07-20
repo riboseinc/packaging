7936830266a917a6ff4b202d19521047cd89cc8e
Jeffrey Lau <jeffrey.lau@ribose.com>
2018-07-20 16:30:38 +0800

    script: Make indentation more consistent

--- scripts/set_yum_push_credentials.sh
+++ scripts/set_yum_push_credentials.sh
@@ -5,27 +5,31 @@ GIT_HTTPS_USERNAME="$1"
 GIT_HTTPS_PASSWORD="$2"
 
 urlencode() {
-    # urlencode <string>
-    old_lc_collate=$LC_COLLATE
-    LC_COLLATE=C
+  # urlencode <string>
+  old_lc_collate=$LC_COLLATE
+  LC_COLLATE=C
 
-    local length="${#1}"
-    for (( i = 0; i < length; i++ )); do
-        local c="${1:i:1}"
-        case $c in
-            [a-zA-Z0-9.~_-]) printf "$c" ;;
-            *) printf '%%%02X' "'$c" ;;
-        esac
-    done
+  local length="${#1}"
+  for (( i = 0; i < length; i++ )); do
+    local c="${1:i:1}"
+    case $c in
+      [a-zA-Z0-9.~_-])
+        printf "$c"
+        ;;
+      *)
+        printf '%%%02X' "'$c"
+        ;;
+    esac
+  done
 
-    LC_COLLATE=$old_lc_collate
+  LC_COLLATE=$old_lc_collate
 }
 
 urldecode() {
-    # urldecode <string>
+  # urldecode <string>
 
-    local url_encoded="${1//+/ }"
-    printf '%b' "${url_encoded//%/\\x}"
+  local url_encoded="${1//+/ }"
+  printf '%b' "${url_encoded//%/\\x}"
 }
 
 git config --global credential.helper store
